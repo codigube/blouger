ad72194ff74adbe90f66f74075f4f91b3056d643
Rui Deng <rui.deng@teliacompany.com>
2020-07-04 23:58:35 +0200

    Setup AWS S3 for hosting a static website

---
+++ infra/aws/main.tf
@@ -0,0 +1,37 @@
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.aws_region
+}
+
+locals {
+  s3_bucket_name = "my-blouger-site-bucket"
+}
+
+resource "aws_s3_bucket" "blouger" {
+  bucket = local.s3_bucket_name
+
+  acl = "public-read"
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+
+  policy = <<EOF
+{
+  "Version": "2008-10-17",
+  "Statement": [
+    {
+      "Sid": "PublicReadForGetBucketObjects",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "*"
+      },
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::${local.s3_bucket_name}/*"
+    }
+  ]
+}
+EOF
+}
