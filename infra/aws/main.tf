d6329b6b77831592df51591a518391b697cea777
Rui Deng <rui.deng@teliacompany.com>
2020-10-08 17:50:43 +0200

    Removed testing code

--- infra/aws/main.tf
+++ infra/aws/main.tf
@@ -46,31 +46,3 @@ resource "aws_s3_bucket" "blouger" {
 }
 EOF
 }
-
-resource "aws_s3_bucket" "blouger1" {
-  bucket = local.s3_bucket_name
-
-  acl = "public-read"
-
-  website {
-    index_document = "index.html"
-    error_document = "error.html"
-  }
-
-  policy = <<EOF
-{
-  "Version": "2008-10-17",
-  "Statement": [
-    {
-      "Sid": "PublicReadForGetBucketObjects",
-      "Effect": "Allow",
-      "Principal": {
-        "AWS": "*"
-      },
-      "Action": "s3:GetObject",
-      "Resource": "arn:aws:s3:::${local.s3_bucket_name}/*"
-    }
-  ]
-}
-EOF
-}
